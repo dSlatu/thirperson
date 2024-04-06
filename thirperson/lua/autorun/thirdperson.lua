b7f4b63a41b6c34696972dd5a82b862cbe480e9e
Slatu <138379901+dSlatu@users.noreply.github.com>
2024-04-06 21:31:26 +0200

    Update thirdperson.lua

--- thirperson/lua/autorun/thirdperson.lua
+++ thirperson/lua/autorun/thirdperson.lua
@@ -1,36 +1,60 @@
 if SERVER then return end
 
-local Touche = KEY_T
+local Touche = KEY_F1
 local DefautPremierPersonne = false
 
+local convarName = "thirdperson_enabled" 
 
-local convar = CreateClientConVar("Slatu 3eme Personne", "1", true, false)
+
+local convar = CreateClientConVar(convarName, "1", true, false)
+
 if DefautPremierPersonne then
     convar:SetBool(false)
 else
     convar:SetBool(true)
 end
-hook.Add("PlayerButtonUp","Module : réaction touche",function(_,action)
+
+
+local function ToggleThirdPerson()
+    convar:SetBool(not convar:GetBool())
+    if convar:GetBool() then
+
+    else
+
+    end
+end
+
+
+concommand.Add("slatu_thirdperson", ToggleThirdPerson)
+
+hook.Add("PlayerButtonUp", "Module: reaction touche", function(_, action)
     if action ~= Touche then return end
-    timer.Create("Temp patch for glitch vision",0.0001,1,function()
-        if not convar:GetBool() then
-            convar:SetBool(true)
-        else
-            convar:SetBool(false)
-        end
+    timer.Simple(0.0001, function() 
+
+        ToggleThirdPerson()
     end)
 end)
 
-hook.Add("CalcView", "Module : affichage du client", function(ply, pos, angles, fov)
+hook.Add("CalcView", "Module: affichage du client", function(ply, pos, angles, fov)
     if convar:GetBool() then
         local view = {
-            origin = pos - ( angles:Forward() * 100 ),
+            origin = pos - (angles:Forward() * 100),
             angles = angles,
             fov = fov,
             drawviewer = true
         }
         return view
-    else
-        return
     end
 end)
+
+local function OnPlayerChat(ply, text, teamChat, isDead)
+
+    if not IsValid(ply) or not ply:IsPlayer() then return end
+
+    if string.lower(text) == "!slatu_thirdperson" then
+    
+        ToggleThirdPerson()
+        return true 
+    end
+end
+hook.Add("OnPlayerChat", "ThirdPersonChatCommand", OnPlayerChat)
